2e7db825bfc54ab8a779fa439053b3bd3e9d4fad
Akash <akashmulye97@gmail.com>
2017-02-05 19:59:55 +0000

    Ah ah ah first function kinda

---
+++ ageCalculator.js
@@ -0,0 +1,8 @@
+function ageCalculator(name, YOB, currentYear) {
+
+	return name + " is " + (currentYear - YOB) + " years old.";
+}
+
+console.log(ageCalculator("Akash", 1997, 2017));
+console.log(ageCalculator("Jihye", 1996, 2017));
+console.log(ageCalculator("Mandem", 1, 2017));
